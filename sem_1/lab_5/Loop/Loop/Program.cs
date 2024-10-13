0cde5100876afb541bfad6b3747334fdea1b9031
Yulia Lapshina <109868318+dymonyx@users.noreply.github.com>
2024-10-13 03:36:47 +0300

    add task1 and 2 for lab5

---
+++ sem_1/lab_5/Loop/Loop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        int[] myArray = { 100, 1, 32, 3, 14, 25, 6, 17, 8, 99 };
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            if (myArray[i] % 2 == 0) myArray[i] = 0;
+            Console.Write(myArray[i] + " ");
+        }
+    }
+}
---
+++ sem_1/lab_5/Loop_update/Loop_update/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        int [] MyArray;
+        int n = int.Parse(Console.ReadLine());
+        MyArray = new int[n];
+        for(int i=0; i < MyArray.Length; ++i)
+        {
+            Console.Write("a[{0}]=",i);
+            MyArray[i] = int.Parse(Console.ReadLine());
+        }
+        for (int i = 0; i < MyArray.Length; i++)
+        {
+            if (MyArray[i] % 2 == 0) MyArray[i] = 0;
+        }
+        foreach (int x in MyArray)Console.Write("{0} ", x);
+    }
+}
